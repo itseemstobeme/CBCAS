b28eb16cd7a6172692c7dbf3b0d1faf36cf06472
Manu <prakhar.kumar0004@gmail.com>
2022-07-08 17:48:49 +0530

    Login Window login implemented

--- CBCAS/LoginWindow.xaml.cs
+++ CBCAS/LoginWindow.xaml.cs
@@ -13,7 +13,8 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace CBCAS
 {
@@ -33,22 +34,82 @@ namespace CBCAS
                 this.DragMove();
         }
 
-        private void TeacherLogin_Click(object sender, RoutedEventArgs e)
-        {
-           
-    
-            this.Close();
-            
-        }
 
+        //When valid login credentials are entered, teacher is logged in
         private void TeacherLoginButton_Click(object sender, RoutedEventArgs e)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["offlineconnectionString"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+            try
+            {
+                mySqlConnection.Open();
+                string cmdString = "SELECT * FROM TEACHERLOGIN WHERE TEACHERID = '" + TeacherID.Text +
+                    "' AND PASSWORD = '" + TeacherPassword.Password + "'";
+                MySqlCommand cmd = new MySqlCommand(cmdString, mySqlConnection);
+                MySqlDataReader mySqlDataReader = cmd.ExecuteReader();
+                if (mySqlDataReader.HasRows)
+                {
+                    TeacherWindow teacherWindow = new TeacherWindow();
+                    teacherWindow.Show();
+                    cmd.Dispose();
+                    mySqlConnection.Close();
 
+                    this.Close();
+                }    
+            }
+            catch
+            {
+                MessageBox.Show("Some Error Occured");
+            }
+        }
+
+        //When valid login credentials are entered, student is logged in
+        private void StudentLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["offlineconnectionString"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+            try
+            {
+                mySqlConnection.Open();
+                string cmdString = "SELECT * FROM STUDENTLOGIN WHERE STUDENTID = '" + StudentID.Text +
+                    "' AND PASSWORD = '" + StudentPassword.Password + "'";
+                MySqlCommand cmd = new MySqlCommand(cmdString, mySqlConnection);
+                MySqlDataReader mySqlDataReader = cmd.ExecuteReader();
+                if (mySqlDataReader.HasRows)
+                {
+                    StudentWindow studentWindow = new StudentWindow();
+                    studentWindow.Show();
+                    cmd.Dispose();
+                    mySqlConnection.Close();
+
+                    this.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some Error Occured");
+            }
         }
 
         private void WINDOWEXIT(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        //For pressing enter after entering password
+        private void TeacherPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Return && e.Key != Key.Enter)
+                return;
+            TeacherLoginButton_Click(sender, e);
+        }
+
+        //For pressing enter after entering password
+        private void StudentPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Return && e.Key != Key.Enter)
+                return;
+            StudentLoginButton_Click(sender, e);
         }
     }
 }
