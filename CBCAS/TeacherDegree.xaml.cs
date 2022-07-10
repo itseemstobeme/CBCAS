975abb2378621d9bc8e4a41fbda8a609d8e8cfb6
Manu <prakhar.kumar0004@gmail.com>
2022-07-10 17:36:32 +0530

    TeacherWindow (after login) navigation implemented

---
+++ CBCAS/TeacherDegree.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+
+namespace CBCAS
+{
+    /// <summary>
+    /// Interaction logic for TeacherBranch.xaml
+    /// </summary>
+    public partial class TeacherDegree : Page
+    {   
+        private string currentYear { get; set; }
+        private TeacherWindow teacherMainWindow { get; set; }
+        public TeacherDegree()
+        {
+            InitializeComponent();
+        }
+        public TeacherDegree(string currentYear,TeacherWindow teacherWindow)
+        {
+            InitializeComponent();
+            this.currentYear = currentYear;
+            this.teacherMainWindow = teacherWindow;
+            initializeButtons(currentYear);
+        }
+
+        //Initializing the year buttons
+        private void initializeButtons(string currentYear)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["offlineconnectionString"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+            try
+            {
+                mySqlConnection.Open();
+                string cmdString = "SELECT DISTINCT DEGREE FROM YEARDEGBRANCH WHERE YEAR = '" + 
+                    currentYear + "'";
+                MySqlCommand cmd = new MySqlCommand(cmdString, mySqlConnection);
+                MySqlDataReader mySqlDataReader = cmd.ExecuteReader();
+                List<string> degreeList = new List<string>();
+                while (mySqlDataReader.Read())
+                {
+                    degreeList.Add(mySqlDataReader.GetString(0));
+                }
+
+                for (int i = 0; i < degreeList.Count; ++i)
+                {
+                    Button button = new Button()
+                    {
+                        Content = degreeList[i],
+                        Tag = degreeList[i],
+                        Style = FindResource("myButtonStyle") as Style
+                    };
+                   button.Click += new RoutedEventHandler(degreeButton_Click);
+                    UfGrid.Children.Add(button);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some Error Occured");
+            }
+        }
+
+        private void degreeButton_Click(object sender, RoutedEventArgs e)
+        {
+            teacherMainWindow.Content = new TeacherBranch(currentYear,(string)(sender as Button).Tag,teacherMainWindow);
+        }
+    }
+}
--- CBCAS/TeacherWindow.xaml.cs
+++ CBCAS/TeacherWindow.xaml.cs
@@ -11,17 +11,108 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace CBCAS
 {
     /// <summary>
     /// Interaction logic for TeacherWindow.xaml
-    /// </summary>
-    public partial class TeacherWindow : Window
+    /// </summary> 
+    public class Teacher
     {
+        public string TeacherID { get; set; }
+        public string TeacherName { get; set; }
+        public Teacher()
+        {
+
+        }
+    }
+
+
+    public partial class TeacherWindow : Window
+    {   
         public TeacherWindow()
         {
             InitializeComponent();
+            TeacherNameLabel.Content +="Ms YEETY BOI";
+            initializeButtons();
+        }
+
+        public TeacherWindow(string TeacherID)
+        {
+            InitializeComponent();
+
+            Teacher teacher = new Teacher();
+            //ADD MYSQL CODE HERE OF GETTING TEACHER INFO
+            TeacherNameLabel.Content += teacher.TeacherName;
+
+            initializeButtons();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+        
+        //Initializing the year buttons
+        private void initializeButtons()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["offlineconnectionString"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+            try
+            {
+                mySqlConnection.Open();
+                string cmdString = "SELECT DISTINCT YEAR FROM YEARDEGBRANCH";
+                MySqlCommand cmd = new MySqlCommand(cmdString, mySqlConnection);
+                MySqlDataReader mySqlDataReader = cmd.ExecuteReader();
+                
+                
+                List<string> yearList = new List<string>();
+                while(mySqlDataReader.Read())
+                {
+                    yearList.Add(mySqlDataReader.GetString(0));
+                }
+
+
+                for(int i = 0;i< yearList.Count;++i)
+                {
+                    Button button = new Button()
+                    { 
+                        Content = yearList[i],
+                        Tag = yearList[i],
+                        /*
+                         Background = Brushes.Transparent,
+                         Margin  = new Thickness(10,10,10,10),
+                         Height  = 70,
+                         Width = 150.5,
+                         BorderThickness = new Thickness(0.5),
+                         FontSize = 25,*/
+                        Style = FindResource("myButtonStyle") as Style
+                    };
+                    button.Click += new RoutedEventHandler(yearButton_click);
+                    wp.Children.Add(button); 
+                    
+                }
+                
+
+                cmd.Dispose();
+                mySqlConnection.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show("Some Error Occured");
+            }
+        }
+
+
+        private void yearButton_click(object sender,RoutedEventArgs e)
+        {
+            //MessageBox.Show((sender as Button).Name);
+            //frame.Content = new TeacherBranch();
+            this.Content = new TeacherDegree((string)(sender as Button).Tag,this);
         }
     }
 }
